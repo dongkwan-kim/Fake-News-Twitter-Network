aac3c2b26cc35b9ffac39168160b4cb576162b4d
Dongkwan Kim <todoaskit@gmail.com>
2018-07-10 12:26:09 +0900

    Bring and refactor codes for data from github.com/todoaskit/HBTP

---
+++ data/format_event.py
@@ -0,0 +1,180 @@
+from format_story import *
+import pandas as pd
+from collections import defaultdict
+import os
+import pprint
+import pickle
+
+EVENT_PATH = './event'
+
+
+def get_event_files():
+    return [os.path.join(EVENT_PATH, f) for f in os.listdir(EVENT_PATH) if 'csv' in f]
+
+
+class FormattedEvent:
+
+    def __init__(self, event_path_list, tweet_id_to_story_id: dict=None, force_save: bool=False):
+        """
+        :param event_path_list: list of str
+        :param tweet_id_to_story_id: dict, str -> int
+        :param force_save: boolean
+        """
+        self.event_path_list = event_path_list
+        self.force_save = force_save
+
+        # Attributes that should be loaded
+        self.parent_to_children: dict = None
+        self.child_to_parent_and_story: dict = None
+        self.story_to_users: dict = None
+        self.user_to_stories: dict = None
+        self.user_to_id: dict = None
+        self.tweet_id_to_story_id: dict = tweet_id_to_story_id
+
+    def get_twitter_year(self):
+        return 'twitter1516'
+
+    def pprint(self):
+        pprint.pprint(self.__dict__)
+
+    def dump(self):
+        file_name = 'FormattedEvent_{}.pkl'.format(self.get_twitter_year())
+        with open(os.path.join(EVENT_PATH, file_name), 'wb') as f:
+            pickle.dump(self, f)
+        print('Dumped: {0}'.format(file_name))
+
+    def load(self):
+        file_name = 'FormattedEvent_{}.pkl'.format(self.get_twitter_year())
+        try:
+            with open(os.path.join(EVENT_PATH, file_name), 'rb') as f:
+                loaded: FormattedEvent = pickle.load(f)
+                self.parent_to_children = loaded.parent_to_children
+                self.child_to_parent_and_story = loaded.child_to_parent_and_story
+                self.story_to_users = loaded.story_to_users
+                self.user_to_stories = loaded.user_to_stories
+                self.user_to_id = loaded.user_to_id
+                self.tweet_id_to_story_id = loaded.tweet_id_to_story_id
+            print('Loaded: {0}'.format(file_name))
+            return True
+        except:
+            print('Load Failed: {0}'.format(file_name))
+            return False
+
+    def get_formatted(self):
+
+        if not self.force_save and self.load():
+            return
+
+        # DataFrame [* rows x 4 columns]
+        events = self.get_events()
+
+        parent_to_children = defaultdict(list)
+        child_to_parent_and_story = defaultdict(list)
+        story_to_users = defaultdict(list)
+        user_to_stories = defaultdict(list)
+
+        user_set = set()
+        story_set = set()
+
+        # Construct a dict from feature to feature
+        for i, event in events.iterrows():
+            # parent: user id or ROOT, user: user id, story: tweet id
+            parent, user, story = map(str, [event['parent_id'], event['user_id'], event['story_id']])
+
+            parent_to_children[parent].append(user)
+            child_to_parent_and_story[user].append((parent, story))
+            story_to_users[story].append(user)
+            user_to_stories[user].append(story)
+
+            user_set.update([parent, user])
+            story_set.add(story)
+
+            if i % 10000 == 0 and __name__ == '__main__':
+                print('events.iterrows: {0}'.format(i))
+
+        # Construct a set of leaf users
+        leaf_users = self.get_leaf_user_set(parent_to_children, user_to_stories)
+
+        # Remove leaf users
+        parent_to_children_final = {parent: [child for child in children if child not in leaf_users]
+                                    for parent, children in parent_to_children.items()}
+        parent_to_children = {parent: children
+                              for parent, children in parent_to_children_final.items()
+                              if len(children) != 0}
+        user_to_stories = {user: story_list
+                           for user, story_list in user_to_stories.items()
+                           if user not in leaf_users}
+        child_to_parent_and_story = {child: parent_and_story
+                                     for child, parent_and_story in child_to_parent_and_story.items()
+                                     if child not in leaf_users}
+        story_to_users = {story: [user for user in user_list if user not in leaf_users]
+                          for story, user_list in story_to_users.items()}
+        user_set = set(user for user in user_set if user not in leaf_users)
+
+        # If self.tweet_id_to_story_id is given, use it. Otherwise use index from sorted(story_set)
+        tweet_id_to_story_id = self.tweet_id_to_story_id \
+                               or dict((story, idx) for idx, story in enumerate(sorted(story_set)))
+        user_to_id = dict((user, idx) for idx, user in enumerate(sorted(user_set)))
+
+        # Indexify
+        self.parent_to_children = self.indexify(parent_to_children, user_to_id, user_to_id)
+        self.child_to_parent_and_story = self.indexify(
+            child_to_parent_and_story, user_to_id, tweet_id_to_story_id, is_c2ps=True
+        )
+        self.story_to_users = self.indexify(story_to_users, tweet_id_to_story_id, user_to_id)
+        self.user_to_stories = self.indexify(user_to_stories, user_to_id, tweet_id_to_story_id)
+
+        self.user_to_id = user_to_id
+
+    def get_events(self) -> pd.DataFrame:
+        events = pd.concat((pd.read_csv(path) for path in self.event_path_list), ignore_index=True)
+
+        # Remove duplicated events
+        events = events.drop(['event_id'], axis=1)
+        events = events.drop_duplicates()
+        events = events.reset_index(drop=True)
+
+        return events
+
+    def get_leaf_user_set(self, parent_to_children, user_to_stories) -> set:
+        leaf_users = set()
+        # parent: str, child: list of str
+        for parent, children in parent_to_children.items():
+            # str
+            for user in children:
+                # leaf_user: No child and Participated story # == 1
+                if user not in parent_to_children and len(user_to_stories[user]) == 1:
+                    leaf_users.add(user)
+        return leaf_users
+
+    def indexify(self, target_dict: dict, key_to_id: dict, value_to_id: dict, is_c2ps=False) -> dict:
+        """
+        :param target_dict: dict {key -> list of values}
+        :param key_to_id: dict
+        :param value_to_id: dict
+        :param is_c2ps: is_child_to_parent_and_story
+        :return: dict {key_to_id[key] -> list(value_to_id[value])}
+        """
+        r_dict = {}
+        for key, values in target_dict.items():
+            if not is_c2ps:
+                r_dict[key_to_id[key]] = list(map(lambda v: value_to_id[v], values))
+            else:
+                # c2ps: key:user -> (key:user, value:story)
+                r_dict[key_to_id[key]] = list(map(lambda v: (key_to_id[v[0]], value_to_id[v[1]]), values))
+        return r_dict
+
+
+def get_formatted_events(tweet_id_to_story_id=None, force_save=False) -> FormattedEvent:
+    fe = FormattedEvent(
+        get_event_files(),
+        tweet_id_to_story_id=tweet_id_to_story_id,
+        force_save=force_save
+    )
+    fe.get_formatted()
+    return fe
+
+
+if __name__ == '__main__':
+    stories = get_formatted_stories()
+    get_formatted_events(stories.tweet_id_to_story_id, force_save=True).dump()
---
+++ data/format_story.py
@@ -0,0 +1,229 @@
+from nltk import PorterStemmer
+import pandas as pd
+import re
+from collections import Counter, defaultdict
+import os
+import pprint
+import pickle
+from copy import deepcopy
+import random
+from pprint import pprint
+
+try:
+    from orderedset import OrderedSet
+except:
+    pass
+
+DATA_PATH = './'
+STORY_PATH = os.path.join(DATA_PATH, 'story')
+
+
+def get_stops():
+    """
+    :return: tuple of two lists: ([...], [...])
+    """
+    stop_words = open(os.path.join(DATA_PATH, 'stopwords.txt'), 'r', encoding='utf-8').readlines()
+    stop_sentences = open(os.path.join(DATA_PATH, 'stopsentences.txt'), 'r', encoding='utf-8').readlines()
+
+    # strip, lower and reversed sort by sentence's length
+    stop_sentences = sorted([ss.strip().lower() for ss in stop_sentences], key=lambda s: -len(s))
+    stop_words = [sw.strip().lower() for sw in stop_words]
+
+    return stop_words, stop_sentences
+
+
+def get_story_files():
+    return [os.path.join(STORY_PATH, f) for f in os.listdir(STORY_PATH) if 'csv' in f]
+
+
+class FormattedStoryElement:
+
+    def __init__(self, story_label: str, word_ids_with_duplicates: list):
+        """
+        :param story_label: str, one of (true, false, non-rumor, unverified)
+        :param word_ids_with_duplicates: list of integer, e.g. [11788, 9185, 10245, 5363, 265, 2871, 3110, ...]
+
+        Attributes
+        ----------
+        self.word_id_to_cnt: dict, int -> int (counts of word)
+        """
+        self.story_label = story_label
+        self.word_id_to_cnt = dict(Counter(word_ids_with_duplicates))
+
+
+class FormattedStory:
+
+    def __init__(self, story_path_list, stemmer=PorterStemmer, delimiter='\s', len_criteria=None, wf_criteria=None,
+                 story_order='original', force_save=False):
+        """
+        Attributes
+        ----------
+
+        :FormattedStoryElement_list: list of FormattedStoryElement
+            - FormattedStoryElement
+                self.story_label: str, one of (true, false, non-rumor, unverified)
+                self.word_id_to_cnt: dict, int -> int (counts of word)
+
+        :word_to_id: dict, str -> int
+
+        :id_to_word: dict, int -> str
+
+        :tweet_id_to_story_id: dict, str (numeric) -> int
+        """
+        self.story_path_list = story_path_list
+        self.stemmer = stemmer()
+        self.delimiter = delimiter
+        self.len_criteria = len_criteria if len_criteria else lambda l: l > 1
+        self.wf_criteria = wf_criteria if wf_criteria else lambda wf: 2 < wf < 500
+        self.stop_words, self.stop_sentences = get_stops()
+        self.force_save = force_save
+
+        # Attributes that should be loaded
+        self.story_order = story_order
+        self.FormattedStoryElement_list: list = None
+        self.word_to_id: dict = None
+        self.id_to_word: dict = None
+        self.tweet_id_to_story_id: dict = None
+
+    def get_twitter_year(self):
+        return 'twitter1516'
+
+    def pprint(self):
+        pprint.pprint(self.__dict__)
+
+    def clone_with_only_mapping(self):
+        tmp = deepcopy(self)
+        tmp.word_ids = defaultdict(list)
+        tmp.word_cnt = defaultdict(list)
+        return tmp
+
+    def remove_stop_sentences(self, content: str):
+        for ss in self.stop_sentences:
+            if ss in content:
+                content = content.replace(ss, '')
+        return content
+
+    def clear_lambda(self):
+        self.len_criteria = None
+        self.wf_criteria = None
+
+    def dump(self):
+        file_name = 'FormattedStory_{}.pkl'.format(self.get_twitter_year())
+        with open(os.path.join(STORY_PATH, file_name), 'wb') as f:
+            self.clear_lambda()
+            pickle.dump(self, f)
+        print('Dumped: {0}'.format(file_name))
+
+    def load(self):
+        file_name = 'FormattedStory_{}.pkl'.format(self.get_twitter_year())
+        try:
+            with open(os.path.join(STORY_PATH, file_name), 'rb') as f:
+                loaded = pickle.load(f)
+                self.FormattedStoryElement_list = loaded.FormattedStoryElement_list
+                self.word_to_id = loaded.word_to_id
+                self.id_to_word = loaded.id_to_word
+                self.tweet_id_to_story_id = loaded.tweet_id_to_story_id
+                self.story_order = loaded.story_order
+            print('Loaded: {0}'.format(file_name))
+            return True
+        except:
+            print('Load Failed: {0}'.format(file_name))
+            return False
+
+    def get_formatted(self):
+
+        if not self.force_save and self.load():
+            return
+
+        stories = pd.concat((pd.read_csv(path) for path in self.story_path_list), ignore_index=True)
+        stories = stories.drop_duplicates(subset=['tweet_id'])
+        stories = stories.reset_index(drop=True)
+
+        # key: int, value: list
+        tweet_id_to_contents = dict()
+
+        # key: int, value: str
+        tweet_id_to_label = dict()
+
+        # key: str, value: int
+        word_frequency = Counter()
+
+        for i, story in stories.iterrows():
+            content = story['title'] + '\n' + story['content']
+            content = content.lower()
+            content = self.remove_stop_sentences(content)
+
+            words = re.split(self.delimiter, content)
+            words = [self.stemmer.stem(v) for v in words]
+            words = [v for v in words if v not in self.stop_words]
+            words = [re.sub('[\W_]+', '', v) for v in words]
+            words = [v for v in words if self.len_criteria(len(v))]
+
+            word_frequency = sum((word_frequency, Counter(words)), Counter())
+
+            tweet_id = str(story['tweet_id'])
+            tweet_id_to_contents[tweet_id] = words
+
+            label = str(story['label'])
+            tweet_id_to_label[tweet_id] = label
+
+            if i % 100 == 0 and __name__ == '__main__':
+                print('stories.iterrows: {0}'.format(i))
+
+        tweet_id_list = list(OrderedSet(tweet_id_to_contents.keys()))
+        if self.story_order == 'shuffle':
+            random.shuffle(tweet_id_list)
+        elif self.story_order == 'sorted':
+            tweet_id_list = sorted(tweet_id_list)
+        elif self.story_order == 'original':
+            pass
+        else:
+            raise NotImplementedError
+
+        tweet_id_to_story_id = dict((tweet_id, idx) for idx, tweet_id in enumerate(tweet_id_list))
+        story_id_to_contents = dict((tweet_id_to_story_id[tweet_id], contents)
+                                    for tweet_id, contents in tweet_id_to_contents.items())
+        story_id_to_label = dict((tweet_id_to_story_id[tweet_id], label)
+                                 for tweet_id, label in tweet_id_to_label.items())
+
+        # Cut by word_frequency
+        for i in story_id_to_contents.keys():
+            words_prev = story_id_to_contents[i]
+            words = [word for word in words_prev if self.wf_criteria(word_frequency[word])]
+            story_id_to_contents[i] = words
+
+        # Construct a set of words
+        vocab = set()
+        for words in story_id_to_contents.values():
+            vocab = vocab | set(words)
+
+        word_to_id = {word: idx for idx, word in enumerate(sorted(vocab))}
+        id_to_word = {idx: word for word, idx in word_to_id.items()}
+        story_id_to_word_ids = {story_id: [word_to_id[word] for word in contents]
+                                for story_id, contents in story_id_to_contents.items()}
+
+        self.FormattedStoryElement_list = [FormattedStoryElement(story_id_to_label[story_id], word_ids)
+                                           for story_id, word_ids in story_id_to_word_ids.items()]
+        self.word_to_id = word_to_id
+        self.id_to_word = id_to_word
+        self.tweet_id_to_story_id = tweet_id_to_story_id
+
+    def get_word_from_id(self, wid):
+        if self.id_to_word:
+            return self.id_to_word[wid]
+        return None
+
+    def get_id_from_word(self, word):
+        if self.word_to_id:
+            return self.word_to_id[word]
+        return None
+
+
+def get_formatted_stories(force_save=False) -> FormattedStory:
+    fs = FormattedStory(get_story_files(), force_save=force_save)
+    fs.get_formatted()
+    return fs
+
+
+if __name__ == '__main__':
+    get_formatted_stories(force_save=True).dump()
