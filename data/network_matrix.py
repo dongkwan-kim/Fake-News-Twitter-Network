29537154760535ab8ee401f09729bd2d014937e1
Dongkwan Kim <todoaskit@gmail.com>
2018-12-15 02:00:11 +0900

    Add user_set and adjacency

--- data/network_matrix.py
+++ data/network_matrix.py
@@ -1,69 +1,295 @@
 from network import *
+from typing import Sequence, Tuple
+from user_set import load_user_set
 import numpy as np
 
+ADJ_PATH = os.path.join(NETWORK_PATH, "adjacency")
 
-class AdjMatrix(np.ndarray):
 
-    # https://docs.scipy.org/doc/numpy-1.13.0/user/basics.subclassing.html
+class AdjMatrix:
 
-    def __init__(self, vertices):
-        return
+    def __init__(self, row_vertices: list, col_vertices: list or None, tuple_key: Tuple,
+                 file_prefix: str = "adj", initial_value: int = -42):
 
-    def __new__(cls, vertices, initial_num=-42):
-        num_vertices = len(vertices)
-        input_array = np.full((num_vertices, num_vertices), initial_num)
-        obj = np.asarray(input_array).view(cls)
-        obj.vertices = np.asarray(vertices)
-        assert len(obj.vertices.shape) == 1
-        return obj
+        self.is_row_col_same = col_vertices is None
 
-    def __array_finalize__(self, obj):
-        # see InfoArray.__array_finalize__ for comments
-        if obj is None:
-            return
-        self.vertices = getattr(obj, 'vertices', None)
+        self.row_vertices = np.asarray(row_vertices)
+        self.col_vertices = self.row_vertices if self.is_row_col_same else np.asarray(col_vertices)
+
+        self.tuple_key = tuple_key
+        self.row_size = len(self.row_vertices)
+        self.col_size = len(self.col_vertices)
+        self.initial_value = initial_value
+        self.file_prefix = file_prefix
+
+        self.arr = np.full((self.row_size, self.col_size), self.initial_value)
+
+    def __getitem__(self, item):
+        return self.arr.__getitem__(item)
+
+    def __repr__(self):
+        return self.arr.__repr__()
 
     def get_u_to_v(self, u, v):
-        u_i, = np.where(self.vertices == u)
-        v_i, = np.where(self.vertices == v)
+        u_i, = np.where(self.row_vertices == int(u))
+        v_i, = np.where(self.col_vertices == int(v))
 
         if len(u_i) != 0 and len(v_i) != 0:
-            return self[u_i[0]][v_i[0]]
+            return self.arr[u_i[0]][v_i[0]]
         else:
             return None
 
     def set_u_to_v(self, u, v, val):
-        u_i, = np.where(self.vertices == u)
-        v_i, = np.where(self.vertices == v)
+        u_i, = np.where(self.row_vertices == int(u))
+        v_i, = np.where(self.col_vertices == int(v))
 
         if len(u_i) != 0 and len(v_i) != 0:
-            self[u_i[0]][v_i[0]] = val
+            self.arr[u_i[0]][v_i[0]] = val
 
-    def dump(self, file):
-        super().dump(os.path.join(NETWORK_PATH, file))
-        with open(os.path.join(NETWORK_PATH, "meta_{}".format(file)), 'wb') as f:
-            pickle.dump({
-                "vertices": self.vertices,
-            }, f)
-        cprint("Dumped AdjMatrix: {}".format(file), "blue")
+    def get_file_name(self, tuple_key):
+        return "{}_{}.pkl".format(self.file_prefix, "_".join([str(e) for e in tuple_key]))
 
-    @classmethod
-    def load(cls, file):
-        loaded = np.load(os.path.join(NETWORK_PATH, file))
-        with open(os.path.join(NETWORK_PATH, "meta_{}".format(file)), 'rb') as f:
+    def dump(self, file=None):
+        file = file if file else self.get_file_name(self.tuple_key)
+        self._arr_dump(file)
+        self._meta_dump(file)
+
+    def load(self, file=None):
+        file = file if file else self.get_file_name(self.tuple_key)
+        self._arr_load(file)
+        self._meta_load(file)
+
+    def _arr_dump(self, file):
+        self.arr.dump(os.path.join(ADJ_PATH, file))
+        cprint("Batch Dumped: {}".format(file), "blue")
+
+    def _arr_load(self, file):
+        loaded = np.load(os.path.join(ADJ_PATH, file))
+        self.arr = loaded
+        cprint("Batch Loaded: {}".format(file), "green")
+        return loaded
+
+    def _meta_load(self, file):
+        with open(os.path.join(ADJ_PATH, "meta_{}".format(file)), 'rb') as f:
             loaded_meta = pickle.load(f)
             for k, v in loaded_meta.items():
-                setattr(loaded, k, v)
-        cprint("Loaded AdjMatrix: {}".format(file), "green")
-        return loaded
+                setattr(self, k, v)
+        cprint("Meta Loaded: {}".format(file), "blue")
+
+    def _meta_dump(self, file):
+        with open(os.path.join(ADJ_PATH, "meta_{}".format(file)), 'wb') as f:
+            pickle.dump({
+                "is_row_col_same": self.is_row_col_same,
+                "row_vertices": self.row_vertices,
+                "col_vertices": self.col_vertices,
+                "initial_value": self.initial_value,
+                "row_size": self.row_size,
+                "col_size": self.col_size,
+                "tuple_key": self.tuple_key,
+                "file_prefix": self.file_prefix,
+            }, f)
+        cprint("Meta Dumped: {}".format(file), "blue")
+
+
+class AdjMatrixAPIWrapper(TwitterAPIWrapper):
+
+    def __init__(self, config_file_path_or_list: str or list,
+                 file_prefix: str = "adj", batch_size: int = 10000, initial_value: int = -42, progress: int = None):
+
+        super().__init__(config_file_path_or_list)
+
+        self.vertices: list = None
+        self.file_prefix = file_prefix
+        self.batch_size = batch_size
+        self.initial_value = initial_value
+        self.row_progress = progress if progress else 0
+
+    def set_vertices(self, vertices, sorting=False):
+        self.vertices = list(vertices) if not sorting else sorted(vertices)
+
+    def _dump_one_batch_matrix(self, one_batch_matrix: AdjMatrix, file_name=None):
+        one_batch_matrix.dump(file_name)
+
+    def _load_one_batch_matrix(self, tuple_key):
+        mat = AdjMatrix(row_vertices=[], col_vertices=[], tuple_key=tuple_key)
+        mat.load()
+        return mat
+
+    def _get_one_batch_matrix(self, row_vertices_batch: list, tuple_key: tuple):
+        mat = AdjMatrix(row_vertices=row_vertices_batch, col_vertices=None, tuple_key=tuple_key,
+                        file_prefix=self.file_prefix, initial_value=self.initial_value)
+
+        for i, u in enumerate(row_vertices_batch):
+            print("one", i)
+            for j, v in enumerate(row_vertices_batch[i:]):
+                u_follows_v, v_follows_u = self.get_sft_and_tfs(u, v)
+                mat[i][j] = u_follows_v
+                mat[j][i] = v_follows_u
+
+        return mat
+
+    def _get_pair_batch_matrix(self, row_vertices_batch: list, col_vertices_batch: list, tuple_key: tuple):
+        mat = AdjMatrix(row_vertices=row_vertices_batch, col_vertices=col_vertices_batch, tuple_key=tuple_key,
+                        file_prefix=self.file_prefix, initial_value=self.initial_value)
+
+        transposed_key = (tuple_key[1], tuple_key[0], tuple_key[2])
+        mat_t = AdjMatrix(row_vertices=col_vertices_batch, col_vertices=row_vertices_batch, tuple_key=transposed_key,
+                          file_prefix=self.file_prefix, initial_value=self.initial_value)
+
+        for i, u in enumerate(row_vertices_batch):
+            print("pair", i)
+            for j, v in enumerate(col_vertices_batch):
+                u_follows_v, v_follows_u = self.get_sft_and_tfs(u, v)
+                mat[i][j] = u_follows_v
+                mat_t[j][i] = v_follows_u
+
+        return mat, mat_t
+
+    def get_matrices(self):
+
+        batch_num = round_up_division(len(self.vertices), self.batch_size)
+
+        for row_idx in range(self.row_progress, batch_num):
+            for col_idx in range(row_idx, batch_num):
+                tuple_key = (row_idx, col_idx, batch_num)
+
+                row_base = row_idx * self.batch_size
+                row_vertices = self.vertices[row_base:row_base + self.batch_size]
+
+                col_base = col_idx * self.batch_size
+                col_vertices = self.vertices[col_base:col_base + self.batch_size]
+
+                if row_idx == col_idx:
+                    mat = self._get_one_batch_matrix(row_vertices, tuple_key)
+                    mat.dump()
+                else:
+                    mat, mat_t = self._get_pair_batch_matrix(row_vertices, col_vertices, tuple_key)
+                    mat.dump()
+                    mat_t.dump()
+
+
+class AdjMatrixFromNetwork:
+
+    def __init__(self, user_id_to_friend_ids: dict, user_id_to_follower_ids: dict,
+                 file_prefix: str = "adj", batch_size: int = 10000, initial_value: int = -42, progress: int = None):
+
+        self.user_id_to_friend_ids = user_id_to_friend_ids
+        self.user_id_to_follower_ids = user_id_to_follower_ids
+
+        self.vertices: list = [int(u) for u in user_id_to_friend_ids.keys() if u != "ROOT"]
+
+        self.file_prefix = file_prefix
+        self.batch_size = batch_size
+        self.initial_value = initial_value
+        self.row_progress = progress if progress else 0
+
+    def _dump_one_batch_matrix(self, one_batch_matrix: AdjMatrix, file_name=None):
+        one_batch_matrix.dump(file_name)
+
+    def _load_one_batch_matrix(self, tuple_key):
+        mat = AdjMatrix(row_vertices=[], col_vertices=[], tuple_key=tuple_key)
+        mat.load()
+        return mat
+
+    def get_sft(self, s, t):
+        friend_ids = self.user_id_to_friend_ids[str(s)]
+        if friend_ids:
+            s_follows_t = int(t in friend_ids)
+        else:
+            s_follows_t = -1
+
+        return s_follows_t
+
+    def _get_batch_matrix(self, row_vertices_batch: list, col_vertices_batch: list, tuple_key: tuple):
+
+        mat = AdjMatrix(row_vertices=row_vertices_batch,
+                        col_vertices=col_vertices_batch,
+                        tuple_key=tuple_key,
+                        file_prefix=self.file_prefix,
+                        initial_value=self.initial_value)
+
+        for i, u in enumerate(row_vertices_batch):
+            for j, v in enumerate(col_vertices_batch):
+                mat[i][j] = self.get_sft(u, v)
+        return mat
+
+    def get_matrices(self):
+
+        batch_num = round_up_division(len(self.vertices), self.batch_size)
+
+        for row_idx in range(self.row_progress, batch_num):
+            for col_idx in range(row_idx, batch_num):
+                tuple_key = (row_idx, col_idx, batch_num)
+
+                row_base = row_idx * self.batch_size
+                row_vertices = self.vertices[row_base:row_base + self.batch_size]
+
+                col_base = col_idx * self.batch_size
+                col_vertices = self.vertices[col_base:col_base + self.batch_size]
+
+                mat = self._get_batch_matrix(row_vertices, col_vertices, tuple_key)
+                print(mat)
+                mat.dump()
+
+
+def get_adj_matrix_from_user_network(friend_file, follower_file, batch_size=10000):
+
+    friend_network = UserNetwork()
+    friend_network.load(friend_file)
+    user_id_to_friend_ids = friend_network.user_id_to_friend_ids
+
+    """
+    follower_network = UserNetwork()
+    follower_network.load(follower_file)
+    user_id_to_follower_ids = follower_network.user_id_to_follower_ids
+    """
+
+    adj_from_network = AdjMatrixFromNetwork(
+        user_id_to_friend_ids=user_id_to_friend_ids,
+        user_id_to_follower_ids={},
+        file_prefix="network_adj",
+        batch_size=batch_size,
+    )
+    adj_from_network.get_matrices()
+
+
+def get_test_user_set():
+    """
+    DF, FB, GG, DK, 404
+    [[ 0  0  1  0 -1]
+     [ 0  0  0  0 -1]
+     [ 1  1  0  0 -1]
+     [ 0  0  1  0 -1]
+     [-1 -1 -1 -1  0]]
+    """
+    return {177143013, 1615618795, 2417413910, 2901434635, 9090909090}
 
 
 if __name__ == '__main__':
-    file_name = "adj_matrix.pkl"
-    mat = AdjMatrix(vertices=range(10000))
-    mat.set_u_to_v(2, 1, 100)
-    print(mat)
-    mat.dump(file_name)
 
-    mat = AdjMatrix.load(file_name)
-    print(mat)
+    MODE = "FROM_NETWORK"
+    start_time = time.time()
+
+    given_config_file_path_list = [os.path.join('config', f) for f in os.listdir('./config') if '.ini' in f]
+
+    if MODE == "TEST":
+        user_set = get_test_user_set()
+        matrix_api = AdjMatrixAPIWrapper(given_config_file_path_list, batch_size=2, file_prefix="test_adj")
+        matrix_api.set_vertices(user_set, sorting=True)
+        matrix_api.get_matrices()
+
+    elif MODE == "TINY":
+        user_set = load_user_set("tiny_one_user_set_follower.pkl")
+        matrix_api = AdjMatrixAPIWrapper(given_config_file_path_list, batch_size=20, file_prefix="tiny_adj")
+        matrix_api.set_vertices(user_set, sorting=False)
+        matrix_api.get_matrices()
+
+    elif MODE == "FROM_NETWORK":
+        get_adj_matrix_from_user_network("UserNetwork_friends.pkl", None)
+
+    total_consumed_secs = time.time() - start_time
+    print('Total {0}h {1}m {2}s consumed'.format(
+        int(total_consumed_secs // 3600),
+        int(total_consumed_secs // 60 % 60),
+        int(total_consumed_secs % 60),
+    ))
---
+++ data/user_set.py
@@ -0,0 +1,157 @@
+import os
+from typing import Set
+
+import pickle
+
+from termcolor import cprint
+
+from network import UserNetwork, NETWORK_PATH
+
+SIZE_LIMIT = 10000 * 10000
+USER_SET_PATH = os.path.join(NETWORK_PATH, "user_set")
+
+
+def dump_user_set(user_set, file):
+    with open(os.path.join(USER_SET_PATH, file), 'wb') as f:
+        pickle.dump(user_set, f)
+    cprint("Dump user set: {} in {} users".format(file, len(user_set)), "blue")
+
+
+def load_user_set(file):
+    with open(os.path.join(USER_SET_PATH, file), 'rb') as f:
+        loaded_user_set = pickle.load(f)
+        cprint("Load user set: {} in {} users".format(file, len(loaded_user_set)), "green")
+        return loaded_user_set
+
+
+def dump_user_set_distributively(user_set, file_prefix, number=3):
+    base = 0
+    size = int(len(user_set)/number) + 1
+    user_list = list(user_set)
+    for i in range(number):
+        sub_user_set = set(user_list[base:base+size])
+        dump_user_set(sub_user_set, f"{file_prefix}_{i}.pkl")
+        base += size
+
+
+def load_user_set_distributively(file_prefix):
+    user_list = []
+    for i, file in enumerate([f for f in os.listdir(USER_SET_PATH) if f.startswith(file_prefix)]):
+        sub_user_set = load_user_set(file)
+        user_list += list(sub_user_set)
+    return set(user_list)
+
+
+def get_unique_user_partition_set_from_network(file_name: str, what_to_crawl: str):
+    user_network = UserNetwork()
+    user_network.load(file_name=file_name)
+
+    user_set: Set[int] = set()
+
+    if file_name:
+        user_id_to_x = user_network.user_id_to_friend_ids
+    else:
+        user_id_to_x = user_network.user_id_to_follower_ids
+
+    total_length = 0
+
+    i = -1
+    for i, (user, f_list) in enumerate(user_id_to_x.items()):
+
+        if f_list is None:
+            user_set.add(int(user))
+            continue
+
+        length = len(user_set)
+
+        if length + len(f_list) >= SIZE_LIMIT:
+            dump_user_set(user_set, "user_set_{}_{}.pkl".format(what_to_crawl, i))
+            user_set = set()
+
+        user_set.add(int(user))
+        user_set.update(f_list)
+        total_length += length + 1
+
+    else:
+        if len(user_set) != 0:
+            dump_user_set(user_set, "user_set_{}_{}.pkl".format(what_to_crawl, i))
+
+    print("Total length: {}".format(total_length))
+
+
+def reduce_user_partition(what_to_crawl, new_limit):
+
+    file_list = sorted([f for f in os.listdir(USER_SET_PATH)
+                        if what_to_crawl in f and f.startswith("user_set")])
+
+    total_user_set = set()
+    i = -1
+    for i, file_name in enumerate(file_list):
+        user_set = load_user_set(file_name)
+        total_user_set.update(user_set)
+        print("Current total_user_set: {}".format(len(total_user_set)))
+
+        if len(total_user_set) >= new_limit:
+            dump_user_set(total_user_set, "reduced_user_set_{}_{}.pkl".format(what_to_crawl, i))
+            total_user_set = set()
+
+    else:
+        if len(total_user_set) != 0:
+            dump_user_set(total_user_set, "reduced_user_set_{}_{}.pkl".format(what_to_crawl, i))
+
+
+def reduce_to_one_and_dump_distributively(what_to_crawl, file_prefix,
+                                          file_prefix_to_merge="reduced_user_set", number=2):
+    file_list = sorted([f for f in os.listdir(USER_SET_PATH)
+                        if what_to_crawl in f and f.startswith(file_prefix_to_merge)])
+
+    total_user_set = set()
+    for i, file_name in enumerate(file_list):
+        user_set = load_user_set(file_name)
+        total_user_set.update(user_set)
+
+    dump_user_set_distributively(total_user_set, file_prefix, number)
+
+
+def get_tiny_user_set(base_file_name, tiny_size):
+    user_set = load_user_set(base_file_name)
+    tiny_user_set = set(list(user_set)[:tiny_size])
+    dump_user_set(tiny_user_set, "tiny_{}".format(base_file_name))
+
+
+def get_user_set_minus_propagated_user_set():
+
+    user_set = load_user_set_distributively("one_user_set")
+
+    user_network = UserNetwork()
+    user_network.load("UserNetwork_friends.pkl")
+    propagated_user_set = set(int(u) for u in user_network.user_id_to_friend_ids)
+
+    dump_user_set_distributively(user_set-propagated_user_set, "not_propagated_user_set_follower", 2)
+
+
+if __name__ == '__main__':
+
+    MODE = "MINUS_PROPAGATED"
+
+    what_to_crawl_in_main = "follower"
+    main_file_name = "UserNetwork_friends.pkl" if what_to_crawl_in_main == "friend" else None
+
+    if MODE == "GET_PARTITION":
+        get_unique_user_partition_set_from_network(main_file_name, what_to_crawl_in_main)
+
+    elif MODE == "REDUCE_PARTITION":
+        reduce_user_partition(what_to_crawl_in_main, 250000000)
+
+    elif MODE == "REDUCE_PARTITION_DIST":
+        reduce_to_one_and_dump_distributively(
+            what_to_crawl_in_main,
+            "one_user_set_{}".format(what_to_crawl_in_main),
+            "one_user_set"
+        )
+
+    elif MODE == "GET_TINY_USER_SET":
+        get_tiny_user_set("one_user_set_follower_0 (201270329).pkl", 1000)
+
+    elif MODE == "MINUS_PROPAGATED":
+        get_user_set_minus_propagated_user_set()
